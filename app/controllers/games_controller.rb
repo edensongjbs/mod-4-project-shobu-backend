fef71a9ca86e027681d8315c2dbedc3e984c8537
edensongjbs <edensongjbs@gmail.com>
2020-08-17 12:32:06 -0400

    new game working

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,9 @@
 class GamesController < ApplicationController
     def index
+        # byebug
         # sends back a string "waiting" if its not user's turn yet, else
         # returns the updated game board and anything else necessary
-        player_id = JWT.decode(game_params[:player_url], "Shobu")[0].to_i
+        player_id = JWT.decode(params[:jwt], "Shobu")[0].to_i
         player = Player.find(player_id)
         game = player.game
         # game = Game.last
@@ -13,19 +14,19 @@ class GamesController < ApplicationController
     end
 
     def create
-        byebug
+        # byebug
         # creates a new game and all associated data.
         # sends back the initial board set up
         # and all data for the pieces
         # send url links for both players
-        player_1_name = games_params[:player_name] || "Player 1"
-        game = Game.create(url:games_params["url"])
+        player_1_name = params[:player_1_name] || "Player 1"
+        player_2_name = params[:player_2_name] || "Player 2"
+        game = Game.create()
         game.generate_current_board
-        if games_params[:player_name] 
-            game.players[0].update_attributes(name: games_params[:player_name])
-        end
+        game.players[0].update_attributes(name: player_1_name)
+        game.players[1].update_attributes(name: player_2_name)
         response = {}
-        response["game"] = game.current_board_json
+        response["game"] = JSON.parse(game.current_board_json)
         response["pieces"] = ShobuSerializer.pieces_serialize(game.pieces)
         response["players"] = {you: ShobuSerializer.player_serialize(game.players[0]), opponent: ShobuSerializer.player_serialize(game.players[1])}
         render json: response
--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -6,7 +6,8 @@ class MovesController < ApplicationController
         #
         # byebug
         the_piece = Piece.find(params[:piece_id].to_i)
-        game = the_piece.player.game
+        player = the_piece.player
+        game = player.game
         start_square = the_piece.current_square
         end_square = game.find_square_by_coords(params[:coordinates])
         Move.create(piece: the_piece, start_square: start_square, end_square: end_square)
