1958a596f3a8702903b2913ea07e6b2dd6dbd4b0
edensongjbs <edensongjbs@gmail.com>
2020-08-18 14:35:44 -0400

    validate player by jwt in movescontroller

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -6,10 +6,13 @@ class MovesController < ApplicationController
         #
         # byebug
         the_piece = Piece.find(params[:piece_id].to_i)
-        player = the_piece.player
+        # player = the_piece.player
+        player_id = JWT.decode(params[:jwt], "Shobu")[0].to_i
+        player = Player.find(player_id)
         game = player.game
         start_square = the_piece.current_square
         end_square = game.find_square_by_coords(params[:coordinates])
+        # byebug
         Move.create(piece: the_piece, start_square: start_square, end_square: end_square)
         game.generate_current_board
         game_specs = {}
